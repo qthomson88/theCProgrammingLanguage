8c99ce083fb75f9f4a38fa1c95b373cee59bfcf9
Quinn <qthomson88@gmail.com>
2023-07-18 22:11:53 -0600

    add explanation of program, TODO of escape(s, t) and simple driver code

---
+++ ch3exercises/ex32/main.c
@@ -0,0 +1,21 @@
+/* This exercise converts white space characters into their ascii escape sequences (e.g. tab becomes \t).
+ * A switch statement should be used */
+
+/* TODO escape(s, t)
+ * while(s(i) !=n '\0'
+ *     switch (inChar)
+ *     case \n
+ *         append '\\' to string t
+ *         append 't' to string t
+ *     case \t
+ *         append '\\' to string t
+ *         append 'n' to string t
+ *     continue for all white space chars
+ *     default
+ *         append s(i) to string t
+ *     append '\0' */
+
+ /* TODO simple driver code
+  * write a phrase that intersperse white space chars with normal chars
+  * send the string, along with another empty string to escape(s, t)
+  * print out first s and then t */
